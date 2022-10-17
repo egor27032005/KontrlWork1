630a6a0fc18ed2b36f55f1380202914d57d93a05
egor27032005 <sergeevegor2005@gmail.com>
2022-10-17 14:16:57 -0400

    написал программу

---
+++ Task1/Program.cs
@@ -0,0 +1,68 @@
+﻿Console.Clear();
+Console.Write("Введите количество строк, которые хотите ввести: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] CreateArrayDouble(int size)
+{
+            string[] strs = new string[n]; //Объявляем массив строк длиной n (которую ввёл пользователь)
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Введите строку №{0}:\r\n    ", i + 1);
+                strs[i] = Console.ReadLine(); //Заполняем его
+            }
+    return strs;
+}
+
+void PrintMatrix(string[] strs)
+{
+    Console.Write("[");
+            for (int i = 0; i < n; i++)
+            {
+                if (i==n-1)
+                Console.Write($"'{strs[i]}'");
+                else
+                Console.Write($"'{strs[i]}',");
+            }
+            Console.WriteLine("]");
+
+}
+string[] calculations(string[] strs,int size)
+   {
+    int sum=0;
+    for (int i = 0; i < size; i++)
+        {
+        string characters = strs[i];
+        int t=characters.Length;
+                if (t<4){
+                sum++;
+                }
+        }      
+    string[] strs2 = new string[sum];
+    int j=0;
+     for (int i = 0; i < size; i++)
+        {
+           string characters = strs[i];
+           int t=characters.Length;
+                if (t<4)
+                {
+                strs2[j] = strs[i];
+                j++;
+                }
+                
+            
+        } 
+    Console.Write("[");
+            for (int i = 0; i < sum; i++)
+            {
+                if (i==sum-1)
+                Console.Write($"'{strs2[i]}'");
+                else
+                Console.Write($"'{strs2[i]}',");
+            }
+            Console.WriteLine("]");  
+   return strs2;
+   }
+   
+String[] array2D=CreateArrayDouble(n);
+PrintMatrix(array2D); 
+String[] array3D=calculations(array2D,n);
+
